02ef7fa912891ef1e3caf852beb8e0d2b6879d43
Vahid Hosseinkhani <20145468@tafe.wa.edu.au>
2025-08-28 13:51:29 +0800

    addedd simple UI

--- pwdgen.py
+++ pwdgen.py
@@ -1,6 +1,10 @@
 import random
+import sys
 
 num_chars = 10
+if len(sys.argv)>1:
+    num_chars=int(sys.argv[1])
+
 password =""
 for index in range(num_chars):
     char_index = random.randint(0,25)
